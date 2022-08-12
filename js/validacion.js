21ac1ae99b274566ae63ce9f9912f1831f8ed371
Nicolás <108936163+nicolasamoroso@users.noreply.github.com>
2022-08-12 09:44:44 -0300

    Se agregó un warning
    Se agrega una nueva alerta en el caso de que se inserten espacios en la contraseña

--- js/validacion.js
+++ js/validacion.js
@@ -1,13 +1,25 @@
+
+                                //---Success Functions---//
 function showAlertSuccess() {
     if (document.getElementById("alert-danger").classList.contains("show"))
         removeAlertError();
+    if (document.getElementById("alert-espacios").classList.contains("show"))
+        removeAlertEspacios();    
     document.getElementById("alert-success").classList.add("show");
     setTimeout(removeAlertSuccess, 5000);
 }
 
+function removeAlertSuccess() {
+    document.getElementById("alert-success").classList.remove("show");
+}
+
+
+                                //---Error Functions---//
 function showAlertError() {
     if (document.getElementById("alert-success").classList.contains("show"))
         removeAlertSuccess();
+    if (document.getElementById("alert-espacios").classList.contains("show"))
+        removeAlertEspacios();    
     document.getElementById("alert-danger").classList.add("show");
     setTimeout(removeAlertError, 5000);
 }
@@ -16,10 +28,23 @@ function removeAlertError() {
     document.getElementById("alert-danger").classList.remove("show");
 }
 
-function removeAlertSuccess() {
-    document.getElementById("alert-success").classList.remove("show");
+
+                               //---Warning Functions---//
+function showAlertWarning() {
+    if (document.getElementById("alert-success").classList.contains("show"))
+        removeAlertSuccess();
+    else if (document.getElementById("alert-danger").classList.contains("show"))
+        removeAlertError();
+    document.getElementById("alert-espacios").classList.add("show");
+    setTimeout(removeAlertEspacios, 5000);
 }
 
+function removeAlertEspacios() {
+    document.getElementById("alert-espacios").classList.remove("show");
+}
+
+
+                                    //---Check---//
 function check() {
     const contra1 = document.getElementById('password1').value
     const contra2 = document.getElementById('password2').value
@@ -27,16 +52,15 @@ function check() {
     const nombre = document.getElementById('nombre').value
     const apellido = document.getElementById('nombre').value
     const terminos = document.getElementById("terminos").checked;
-
     const noValido = / /;
+
     if (!nombre  || !apellido || !email || !contra1 || !contra2  || !terminos 
         || contra1 !== contra2 
-        || contra1.length < 6
-        || noValido.test(contra1) || noValido.test(contra2)){
+        || contra1.length < 6){
         showAlertError();
-    }else{
-        showAlertSuccess();
     }
-
-
+    else if (noValido.test(contra1) || noValido.test(contra2))
+        showAlertWarning();
+    else
+        showAlertSuccess();
 }
