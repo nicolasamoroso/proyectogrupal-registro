c834be3f58f3f21bcfcf079c3efce66fcbcd12c5
florenciabueno <florenxiabueno@gmail.com>
2022-08-12 08:42:53 -0300

    Update validacion.js
    validacion de contraseñas

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,14 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+function check {
+    
+    let contra = getElementById(password1).value
+
+    let contra2 = getElementById(password2).value
+
+    if (contra === contra2 )
+
+    
+}
